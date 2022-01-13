7c3fc1c7a865b08990371ae9a6a1195e1c6ba9a1
Timofey Lyadinsky <tima.led12@gmail.com>
2022-01-13 20:02:33 +0300

    feat: add solution in index js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let rez = 0;
+    let tmp = 1;
+    if(n<0) n=-n
+    while(n>tmp){
+      tmp*=10;
+    }
+    tmp/=10;
+    while(n>=1){
+        rez+=n%10*tmp;
+        tmp/=10;
+        n=Math.floor(n/10)
+    }
+  return rez;
 }
